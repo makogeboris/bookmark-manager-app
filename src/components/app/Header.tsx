6a44d5d0f2c6f1a2a86e638b8cbb9dbb30e2cfa4
Makoge Boris <makogeboris@gmail.com>
2026-05-29 06:32:29 +0100

    feat: dialogs layout

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -2,6 +2,7 @@ import SearchBar from "./SearchBar";
 import AvatarDropdown from "./AvatarDropdown";
 import { Button } from "../ui/button";
 import { Icons } from "./Icons";
+import AddBookmark from "./AddBookmark";
 
 interface HeaderProps {
   onMenuClick?: () => void;
@@ -10,10 +11,10 @@ interface HeaderProps {
 export default function Header({ onMenuClick }: HeaderProps) {
   return (
     <header>
-      <div className="flex items-center justify-between py-3 sm:py-4 px-4 bg-sidebar sm:px-8 gap-2.5 border-sidebar-border border-b">
-        <div className="flex items-center gap-2.5 sm:gap-4 w-full">
+      <div className="bg-sidebar border-sidebar-border flex items-center justify-between gap-2.5 border-b px-4 py-3 sm:px-8 sm:py-4">
+        <div className="flex w-full items-center gap-2.5 sm:gap-4">
           <Button
-            className="lg:hidden sm:size-11"
+            className="sm:size-11 lg:hidden"
             size="icon-lg"
             variant="outlineMenu"
             onClick={onMenuClick}
@@ -24,14 +25,15 @@ export default function Header({ onMenuClick }: HeaderProps) {
         </div>
 
         <div className="flex items-center gap-2.5 sm:gap-4">
-          <Button size="icon-lg" className="md:hidden">
+          <AddBookmark />
+          {/* <Button size="icon-lg" className="md:hidden">
             {Icons.plus}
           </Button>
 
           <Button className="hidden md:flex" size="xxl">
             {Icons.plus}
             <span>Add Bookmark</span>
-          </Button>
+          </Button> */}
           <AvatarDropdown />
         </div>
       </div>
